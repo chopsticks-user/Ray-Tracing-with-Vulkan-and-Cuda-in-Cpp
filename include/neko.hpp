6db96cee82563013c7ed73cb3ba2b24c6e1b4190
Frost <frostyfrost273@gmail.com>
2022-12-13 21:03:57 -0500

    Make some changes in CMake files

--- include/neko.hpp
+++ include/neko.hpp
@@ -1,9 +1,9 @@
 #ifndef NEKO_HPP
 #define NEKO_HPP
 
-#include "../src/engine/engine.hpp"
-#include "../src/events/events.hpp"
-#include "../src/renderer/renderer.hpp"
-#include "../src/threads/threads.hpp"
+#include "engine/engine.hpp"
+#include "events/events.hpp"
+#include "renderer/renderer.hpp"
+#include "threads/threads.hpp"
 
 #endif /* NEKO_HPP */
